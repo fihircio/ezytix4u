68cc45c20cbcee88bbb9f52b3be9b842d6419064
fihircio <fihir.omar@gmail.com>
2024-07-21 22:18:17 +0800

    up

--- eventmie-pro/resources/js/events_manage/components/FileUpload.vue
+++ eventmie-pro/resources/js/events_manage/components/FileUpload.vue
@@ -9,8 +9,8 @@
                     </div>
                     <div class="modal-body">
                         <form ref="form" @submit.prevent="validateForm" method="POST" enctype="multipart/form-data">
-                            <input type="hidden" class="form-control lgxname" name="ticket_id" v-model="local_ticket.id">
-                            <input type="hidden" class="form-control lgxname" name="event_id" v-model="local_ticket.event_id">
+                            <input type="hidden" class="form-control lgxname"  name="ticket_id" v-model="local_ticket.id">
+                            <input type="hidden" class="form-control lgxname"  name="event_id" v-model="local_ticket.event_id">
                             
                             <div class="form-group col-md-4 text-wrap">
                                 <h4 class="mb-2 form-label">{{ trans('em.upload_seatchart') }}</h4>
@@ -23,66 +23,75 @@
             </div>
         </div>
     </div>
-</template>
 
+</template>
+  
 <script>
-import { mapState, mapMutations } from 'vuex';
+import route from 'ziggy-js';
 import mixinsFilters from '../../mixins.js';
 import SeatComponent from './Seat.vue';
 
 export default {
-    props: ['ticket'],
-    mixins: [mixinsFilters],
+
+    props:['ticket'],
+    mixins:[
+        mixinsFilters
+    ],
+
     components: {
         SeatComponent,
     },
-    data() {
+
+    data(){
         return {
-            local_ticket: this.ticket,
-        };
+            local_ticket : this.ticket,   
+        }
     },
-    computed: {
-        ...mapState({
-            file: state => state.file,
-            event_id: state => state.event_id,
-            organiser_id: state => state.organiser_id,
-        }),
-    },
+  
     methods: {
-        ...mapMutations(['add', 'update']),
-        onChangeFileUpload() {
+        
+        // on change file upload
+        onChangeFileUpload(){
             let formData = new FormData(this.$refs.form);
+
             formData.append('file', this.$refs.file.files[0]);
 
-            axios.post(route('eventmie.upload_seatchart'), formData)
-                .then(res => {
-                    this.local_ticket = res.data.ticket;
-                    this.showNotification('success', trans('em.seatchart_uploaded'));
-                    this.add({ ticket: this.local_ticket }); // Update the Vuex state if necessary
-                })
-                .catch(error => {
-                    let serrors = Vue.helpers.axiosErrors(error);
-                    if (serrors.length) {
-                        this.serverValidate(serrors);
-                    }
-                });
-        },
-        validateForm() {
-            this.$validator.validateAll().then((result) => {
-                if (result) {
-                    this.saveSeat();
+            axios.post(route('eventmie.upload_seatchart'),
+                formData
+            ).then(res => {
+                this.local_ticket = res.data.ticket; 
+
+                this.showNotification('success', trans('em.seatchart_uploaded'));
+            })
+            .catch(error => {
+                let serrors = Vue.helpers.axiosErrors(error);
+                if (serrors.length) {
+                    this.serverValidate(serrors);
                 }
             });
         },
+
+        // validate data on form submit
+        validateForm(event) {
+            this.$validator.validateAll().then((result) => {
+                if (result) {
+                    this.saveSeat(event);            
+                }
+            });
+        },
+
+        // show server validation errors
         serverValidate(serrors) {
-            this.$validator.validateAll().then(() => {
+            this.$validator.validateAll().then((result) => {
                 this.$validator.errors.add(serrors);
             });
         },
-        close() {
+
+        close(){
             this.$parent.getTickets();
             this.$parent.seat_ticket_id = 0;
         }
+    
     }
-};
+  }
 </script>
