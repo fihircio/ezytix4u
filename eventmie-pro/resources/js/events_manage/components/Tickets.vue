b8e403e989dfc0bb873f751436825457082bbcf3
fihircio <fihir.omar@gmail.com>
2024-07-22 19:20:35 +0800

    test

--- eventmie-pro/resources/js/events_manage/components/Tickets.vue
+++ eventmie-pro/resources/js/events_manage/components/Tickets.vue
@@ -59,6 +59,7 @@ import { mapState, mapMutations} from 'vuex';
 import TicketComponent from './Ticket.vue';
 import mixinsFilters from '../../mixins.js';
 import FileUpload from './FileUpload.vue';
+import SeatComponent from './Seat.vue';
 
 export default {
 
@@ -79,6 +80,7 @@ export default {
     components: {
         TicketComponent,
         FileUpload,
+        SeatComponent,
     },
     computed: {
         // get global variables
